5d638855f74ee9de4d2592c93629ff3c7f7f15f2
Chermaine Zimmerman <C14jcdj@yahoo.com>
2015-06-28 13:44:44 -0700

    wip2

--- js/index.js
+++ js/index.js
@@ -1,29 +1,12 @@
 (function(){
-	$(function(){
-		// var strength1 = 50;
-		// var strength2 = 100;
-		// var strength3 = 200;
-		// $("html").mousemove(function(e){
-		//     var pageX = e.pageX - ($(window).width() / 2);
-		//     var pageY = e.pageY - ($(window).height() / 2);
-		//     var newvalueX = 1* pageX * -1;
-		//     var newvalueY = 1* pageY * -1;
-		//     $('#background').css("background-position", (strength1 / $(window).width() * pageX * -1)+"px "+(strength1  / $(window).height() * pageY * -1)+"px");
-		//     $('#middleground').css("background-position", (strength2 / $(window).width() * pageX * -1)+"px "+(strength2  / $(window).height() * pageY * -1)+"px");
-		//     $('#foreground').css("background-position", (strength3 / $(window).width() * pageX * -1)+"px "+(strength3  / $(window).height() * pageY * -1)+"px");
-		// });
-
+	
 	function parallax() {
 	    setpos("#pb0");
 	    setpos("#pb1");
 	    setpos("#pb2", 4);
 	    setpos("#pb3");
 	    setpos("#pb4");
-	    // $('.chewy1').css('position', 'relative')
-	    // $('.chewy1').css('top', '800px')
 	}
-
-
 
 	function setpos(element, factor) {
 	    factor = factor || 2;
@@ -40,22 +23,44 @@
 	    // $(element).css('background-position', posx+'px '+posy+'px');
 	}
 
-	function displayMainPage(){
-
-	function showElements(){
-		timeoutID2 = window.setTimeout(showSection, 1000);
-		
-	}
 	function showSection(){
 		$('#one').show().addClass('animated slideInLeft')
 		$('#two').show().addClass('animated slideInRight')
 		$('#three').show().addClass('animated slideInLeft')
 	}
 
-	timeoutID = window.setTimeout(showElements, 2000);
-}
+	$(function(){
+		var strength1 = 50;
+		var strength2 = 100;
+		var strength3 = 200;
+		$("html").mousemove(function(e){
+		    var pageX = e.pageX - ($(window).width() / 2);
+		    var pageY = e.pageY - ($(window).height() / 2);
+		    $('#background').css("background-position", (strength1 / $(window).width() * pageX * -1)+"px "+(strength1  / $(window).height() * pageY * -1)+"px");
+		    $('#middleground').css("background-position", (strength2 / $(window).width() * pageX * -1)+"px "+(strength2  / $(window).height() * pageY * -1)+"px");
+		    $('#foreground').css("background-position", (strength3 / $(window).width() * pageX * -1)+"px "+(strength3  / $(window).height() * pageY * -1)+"px");
+		});
 
-// displayMainPage()
+	
+
+
+
+	
+
+	$('#navlist a').click(function() {
+      var target = $(this.hash);
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 2000);
+        return false;
+      }
+  });
+
+
+	
+	timeoutID = window.setTimeout(showSection, 2000);
+
 
 	var oldPageYOffset = 0;
 	$(window).scroll(function(){
@@ -63,7 +68,6 @@
 	  {
 	    oldPageYOffset = window.pageYOffset;
 	    parallax()
-	    console.log("Window scrolling changed");
 	  }
 	});
 	
