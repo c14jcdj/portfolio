dcac6237bf278f352beb4d79fb30841bc1d9686d
Chermaine Zimmerman <C14jcdj@yahoo.com>
2015-07-12 22:11:32 -0700

    finished

--- js/_main.js
+++ js/_main.js
@@ -31,32 +31,67 @@
 	function adjustWindow(){
 		
 		// Init Skrollr
-		var s = skrollr.init({
-		    forceHeight: false,
-		    render: function(data) {
-		    
-		        //Debugging - Log the current scroll position.
-		        //console.log(data.curTop);
-		    }
-		});
+		// var s = skrollr.init({
+		//     forceHeight: false,
+		//     render: function(data) {
+		//     }
+		// });
 		
 		// Get window size
 	    winH = $window.height();
-	    
+	    winW = $window.width();
 	    // Keep minimum height 550
 	    if(winH <= 550) {
 			winH = 550;
 		} 
+
+		// Init Skrollr for 768 and up
+		    // if( winW >= 768) {
+		    if( !Modernizr.touch) {
+		 
+		        // Init Skrollr
+		        var s = skrollr.init({
+		            forceHeight: false
+		        });
+		 
+		        // // Resize our slides
+		        // $slide.height(winH);
+		 
+		        // s.refresh($('.homeSlide'));
+
+		        // Resize our slides
+		        $slide.height(winH);
+		        $slideTall.height(winH*2);
+		        $slideTall2.height(winH*3);
+		        
+		        // Refresh Skrollr after resizing our sections
+		        s.refresh($('.homeSlide'));
+		 
+		    } else {
+		 
+		        $('#desktop').hide();
+		        $('#mobile').show();
+		        
+		        var s = skrollr.init({
+		            forceHeight: false
+		        });
+
+		 
+		        
+		        // Resize our slides
+		        $slide.height(winH);
+		        $slideTall.height(winH*1.5);
+		        $slideTall2.height(winH*2.5);
+		        
+		        // Refresh Skrollr after resizing our sections
+		        s.refresh($('.homeSlide'));
+
+		    }
+
 	    
-	    // Resize our slides
-	    $slide.height(winH);
-	    $slideTall.height(winH*2);
-	    $slideTall2.height(winH*3);
-	    
-	    // Refresh Skrollr after resizing our sections
-	    s.refresh($('.homeSlide'));
 	    
 	}
+
 
 	function ModalInfo(){
 		this.projects = {
@@ -135,9 +170,8 @@
 			 	link = modalInfo.projects[project].link,
 			 	list = modalInfo.projects[project].list;
 			modalInfo.applyTextToModal(title, image, list, stack, link)
-			
-
 		})
+
 	}
 
 	function hideName(){
@@ -164,6 +198,30 @@
 			        scrollTop: $("#footer").offset().top 
 			    }, 2000);
 		})
+		$('#about-me-mobile').on('click', function () {
+			var s = skrollr.init({
+		            forceHeight: false
+		        });
+			var target = $("#about-me-target-mobile").offset().top
+			s.animateTo(target)
+			
+		})
+		$('#projects-mobile').on('click', function () {
+			var s = skrollr.init({
+		            forceHeight: false
+		        });
+			var target = $("#header").offset().top
+			s.animateTo(target)
+			
+		})
+		$('#contact-me-mobile').on('click', function () {
+			var s = skrollr.init({
+		            forceHeight: false
+		        });
+			var target = $("#footer-mobile").offset().top
+			s.animateTo(target)
+		})
+		
 	}
 
 	bindModalConstructor()
